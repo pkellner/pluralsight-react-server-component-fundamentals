1ec988878029bbb8b9b59e2e42261dd688399a19
pkellner <peter@peterkellner.net>
2023-04-24 15:24:07 -0700

    go

--- m2/clip-11/src/app/app-show-theme.js
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { ThemeContext } from './app-theme-provider';
-import { useContext } from 'react';
-
-export default function AppShowTheme() {
-  const { darkTheme } = useContext(ThemeContext);
-  return <div>{darkTheme ? "Dark" : "Light"} theme</div>;
-}
