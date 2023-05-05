99107b77e9573d2ae9065af21155037cac32c93a
pkellner <peter@peterkellner.net>
2023-05-04 18:25:42 -0700

    x

---
+++ m4/clip-060-final/src/app/sessions/sessions-list.tsx
@@ -0,0 +1,37 @@
+import 'server-only';
+
+import { SessionData } from "@/lib/ts-interfaces";
+import SessionListItem from "@/src/app/sessions/session-list-item";
+
+import sessionsData from "../../../data/sessions.json";
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function getSessionsList() {
+  await delay(2000);
+  return sessionsData.data.sessions;
+}
+
+// async function getSessionsList() {
+//   await delay(2000);
+//   const res = await fetch(`http://localhost:3000/api/sessiondata?max=11`);
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   const data = await res.json();
+//   return data;
+// }
+
+export default async function SessionsList() {
+  const sessionData = await getSessionsList();
+  console.log(sessionData.length);
+  return (
+    <div className="container">
+      <div className="row">
+        {sessionData.map(function (rec: SessionData) {
+          return <SessionListItem key={rec.id} rec={rec} />;
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ m4/old/clip-010-final-mostly-client-components/src/app/sessions/sessions-list.tsx
@@ -0,0 +1,37 @@
+"use client";
+import {SessionData} from "@/lib/ts-interfaces";
+import SessionListItem from "@/src/app/sessions/session-list-item";
+import {useEffect, useState} from "react";
+export default function SessionsList() {
+  const [sessions, setSessions] = useState<SessionData[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/api/sessiondata?max=11"
+        );
+        const data = await response.json();
+        setSessions(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData().then(() => console.log("done"));
+  }, []);
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
+    <div className="container">
+      <div className="row">
+        {sessions?.map(function (rec: SessionData) {
+          return <SessionListItem key={rec.id} rec={rec} />;
+        })}
+      </div>
+    </div>
+  );
+}
