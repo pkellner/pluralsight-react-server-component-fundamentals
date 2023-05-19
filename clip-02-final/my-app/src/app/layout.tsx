168d03d03031b5374b3f9086eda98af01ee08cda
pkellner <peter@peterkellner.net>
2023-05-19 15:34:40 -0700

    x

--- clip-02-final/my-app/src/app/layout.tsx
+++ clip-02-final/my-app/src/app/layout.tsx
@@ -1,15 +1,19 @@
+import "./globals.css";
+import { ReactNode } from "react";
 
-import './globals.css'
-import {ReactNode} from "react";
-
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div style={{ overflow: "hidden" }}>
+          <header className="header">
+            <div className="container-main">
+              <div>NAV</div>
+            </div>
+          </header>
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
