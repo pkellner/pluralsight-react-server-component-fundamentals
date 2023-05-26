391e273a222b078998675533be374740e48838d2
pkellner <peter@peterkellner.net>
2023-05-25 17:55:48 -0700

    x

--- m6/clip-05/src/app/page.tsx
+++ m6/clip-05/src/app/page.tsx
@@ -1,13 +1,40 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { sessionsData } from "@/app/common/sessions-data";
+import SpeakerDetail from "@/app/speaker-detail";
 
-export default function Home() {
+export interface Session {
+  id?: string;
+  title?: string;
+  speakerId?: string;
+  description?: string;
+}
+
+async function getSessions() {
+  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+  await delay(2000); // 2 seconds
+  return sessionsData;
+}
+
+export default async function Sessions() {
+  const sessions = await getSessions();
   return (
     <div className="container-main">
-      <h4>Douglas Crockford</h4>
-      <div className="events-speaker-photo">
-        <Image src="/speakers/speaker-1124.jpg"
-          alt="Douglas Crockford"
-          width={135} height={135} />
+      <div className="sessions">
+        <ul className="news-list">
+          {sessions.map((session: Session) => {
+            return (
+              <li key={session.id} className="news-tile">
+                <div className="news-tile__top">
+                  <h3 className="news-tile__title">{session?.title}</h3>
+                  {session?.description}
+                </div>
+                <div className="news-tile__bottom">
+                  <SpeakerDetail speakerId={session.speakerId ?? "0"} />
+                </div>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </div>
   );
