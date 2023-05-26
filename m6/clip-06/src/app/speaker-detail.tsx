91c48aca7f9a4b7f22aa1d4e716de674c0df8506
pkellner <peter@peterkellner.net>
2023-05-25 18:27:07 -0700

    x

---
+++ m6/clip-06/src/app/speaker-detail.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { speakersData } from "@/app/common/speakers-data";
+import 'server-only';
+
+export interface Speaker {
+  id?: string;
+  first?: string;
+  last?: string;
+  bio?: string;
+  sessionId?: string;
+}
+
+async function getSpeaker(speakerId: string) {
+  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+  await delay(2000);
+  return (speakersData.find((speaker: Speaker) => speaker.id === speakerId) || {});
+}
+
+export default async function SpeakerDetail({ speakerId }: {
+  speakerId: string;
+}) {
+  const speaker: Speaker = await getSpeaker(speakerId);
+  return (
+    <div className="col-12-col-sm-6 speakers-list-item" key={speakerId}>
+      <div className="events-speaker d-flex align-items-center">
+        <div className="events-speaker-photo">
+          <Image
+            src={`/speakers/speaker-${speaker?.id}.jpg`}
+            alt={`${speaker.first} ${speaker.last}`}
+            width={135} height={135} />
+        </div>
+        <div className="events-speaker-description">
+          <a href="#">
+            <div className="name">
+              {speaker.first} {speaker.last}
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
