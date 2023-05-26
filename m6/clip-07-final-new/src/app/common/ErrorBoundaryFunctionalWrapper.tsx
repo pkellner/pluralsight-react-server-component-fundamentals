5cc83207b2f2287fabbf8efb8b9ebc34d9aaccfb
pkellner <peter@peterkellner.net>
2023-05-26 06:20:19 -0700

    x

---
+++ m6/clip-07-final-new/src/app/common/ErrorBoundaryFunctionalWrapper.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default function ErrorBoundaryFunctionalWrapper({
+  children,
+}: {
+  children: ReactNode;
+}) {
+
+  function Fallback({ error }: { error: Error }) {
+    return (
+      <div role="alert">
+        <p>Something went wrong:</p>
+        <pre style={{ color: "red" }}>{error.message}</pre>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {
+        /* @ts-ignore */
+        <ErrorBoundary FallbackComponent={Fallback}>
+          {children}
+        </ErrorBoundary>
+      }
+    </>
+  );
+}
