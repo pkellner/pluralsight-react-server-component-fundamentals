8245e55760f0f519d7126d63ae7207d1aeea1ee9
pkellner <peter@peterkellner.net>
2023-06-13 16:44:00 -0700

    update

--- m7/clip-060-final/next.config.js
+++ m7/clip-060-final/next.config.js
@@ -3,12 +3,4 @@
 
 // module.exports = nextConfig;
 
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    //ignoreBuildErrors: true,
-  },
-};
+module.exports = {};
