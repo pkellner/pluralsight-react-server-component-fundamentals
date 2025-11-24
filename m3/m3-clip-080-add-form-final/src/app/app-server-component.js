9639b337e0dd0971dadc93cd9047b495a25c46f5
pkellner <peter@peterkellner.net>
2025-11-24 06:13:15 -0800

    update

--- m3/m3-clip-080-add-form-final/src/app/app-server-component.js
+++
@@ -1,9 +0,0 @@
-import "server-only";
-
-export default function AppServerComponent() {
-  return (
-    <div className="container border">
-      <h2>I'm a Server Component</h2>
-    </div>
-  );
-}
